e9e33a3079a8f4b7da4f36a854c84f375b534abc
m-owada <m.owada.0629@live.jp>
2025-03-19 01:44:29 +0900

    modified

--- main.js
+++ main.js
@@ -20,12 +20,26 @@ var board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
 var record = [];
-var turn = 0;
 
-var spStone = [];
-var spButton1 = [];
-var spButton2 = [];
-var spMessage = [];
+var kou =
+{
+    turn: 0,
+    x: 0,
+    y: 0
+};
+
+var prisoner =
+{
+    black: 0,
+    white: 0
+};
+
+var objStone = [];
+var objButton1 = [];
+var objButton2 = [];
+var objMessage = [];
+
+var timedEvent = null;
 
 mainScene.preload = function()
 {
@@ -42,13 +56,13 @@ mainScene.create = function()
     
     for(var x = 0; x < board.length; x++)
     {
-        spStone[x] = [];
+        objStone[x] = [];
         for(var y = 0; y < board[x].length; y++)
         {
-            spStone[x][y] = this.add.sprite(x * 24 + 16, y * 24 + 16, "stone").setInteractive();
-            spStone[x][y].posX = x;
-            spStone[x][y].posY = y;
-            spStone[x][y].on("pointerdown", clickStone);
+            objStone[x][y] = this.add.sprite(x * 24 + 16, y * 24 + 16, "stone").setInteractive();
+            objStone[x][y].posX = x;
+            objStone[x][y].posY = y;
+            objStone[x][y].on("pointerdown", clickStone);
         }
     }
     
@@ -64,28 +78,25 @@ mainScene.create = function()
     button2.on("pointerdown", clickButton2);
     
     
-    for(var x = 0; x < 7; x++)
+    for(var i = 0; i < 7; i++)
     {
-        spButton1[x] = this.add.sprite(x * 16 +  40, 336, "string");
-        spButton2[x] = this.add.sprite(x * 16 + 184, 336, "string");
+        objButton1[i] = this.add.sprite(i * 16 +  40, 336, "string");
+        objButton2[i] = this.add.sprite(i * 16 + 184, 336, "string");
     }
     
-    
-    for(var x = 0; x < 17; x++)
+    var i = 0;
+    for(var y = 0; y < 3; y++)
     {
-        spMessage[x] = [];
-        for(var y = 0; y < 3; y++)
+        for(var x = 0; x < 17; x++)
         {
-            spMessage[x][y] = this.add.sprite(x * 16 + 32, y * 26 + 390, "string");
-            spMessage[x][y].setFrame(0);
+            objMessage[i] = this.add.sprite(x * 16 + 32, y * 26 + 390, "string");
+            i++;
         }
     }
     
-    setMessage("＊「どちらにしますか");
+    setMessage("くろのばんです。");
     setButton1("　　く　ろ");
     setButton2("　　し　ろ");
-    
-    
 }
 
 mainScene.update = function()
@@ -105,61 +116,69 @@ var clickButton2 = function()
 
 var clickStone = function()
 {
-    if(board[this.posX][this.posY] == 0)
+    var color = getColor();
+    if(checkLegal(color, this.posX, this.posY))
     {
-        if(turn % 2 == 0)
+        setStone(color, this.posX, this.posY);
+        setRecord(color, this.posX, this.posY);
+        
+        if(color == 1)
         {
-            board[this.posX][this.posY] = 1;
+            setMessage("しろのばんです。");
         }
         else
         {
-            board[this.posX][this.posY] = 2;
+            setMessage("くろのばんです。");
         }
-        this.setFrame(board[this.posX][this.posY]);
-        
-        record[turn] =
-        {
-            color: board[this.posX][this.posY],
-            x: this.posX,
-            y: this.posY
-        };
-        
-        
-        
-        
-        turn++;
     }
-}
-
-var setMessage = function(mes)
-{
-    var i = 0;
-    for(var y = 0; y < 3; y++)
+    else
     {
-        for(var x = 0; x < 17; x++)
-        {
-            spMessage[x][y].setFrame(getCharList().indexOf(mes.charAt(i)));
-            i++;
-        }
+        setMessage("ここにいしはおけません。");
     }
 }
 
-var setButton1 = function(mes)
+
+var setMessage = function(message)
+{
+    if(timedEvent != null)
+    {
+        timedEvent.remove(false);
+    }
+    
+    for(var i = 0; i < objMessage.length; i++)
+    {
+        objMessage[i].setFrame(0);
+    }
+    
+    var i = 0;
+    timedEvent = mainScene.time.addEvent(
+    {
+        delay: 50,
+        callback: () =>
+        {
+            objMessage[i].setFrame(getCharList().indexOf(message.charAt(i)));
+            i++;
+        },
+        repeat: objMessage.length - 1
+    });
+}
+
+var setButton1 = function(message)
 {
     var i = 0;
     for(var x = 0; x < 7; x++)
     {
-        spButton1[x].setFrame(getCharList().indexOf(mes.charAt(i)));
+        objButton1[x].setFrame(getCharList().indexOf(message.charAt(i)));
         i++;
     }
 }
 
-var setButton2 = function(mes)
+var setButton2 = function(message)
 {
     var i = 0;
     for(var x = 0; x < 7; x++)
     {
-        spButton2[x].setFrame(getCharList().indexOf(mes.charAt(i)));
+        objButton2[x].setFrame(getCharList().indexOf(message.charAt(i)));
         i++;
     }
 }
@@ -171,6 +190,317 @@ var getCharList = function()
            "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン" +
            "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヴャュョッァィゥェォ" + 
            "＊「…！？♪ー。ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ１２３４５６７８９０＿";
+}
+
+var getTurn = function()
+{
+    return record.length + 1;
+}
+
+var getColor = function()
+{
+    if(getTurn() % 2 != 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 2;
+    }
+}
+
+var setRecord = function(color, x, y)
+{
+    record[record.length] =
+    {
+        color: color,
+        x: x,
+        y: y
+    };
+}
+
+var checkLegal = function(color, x, y)
+{
+    if(board[x][y] != 0)
+    {
+        return false;
+    }
+    
+    if(getTurn() > 1)
+    {
+        if(kou.x == x && kou.y == y && kou.turn == getTurn() - 1)
+        {
+            return false;
+        }
+    }
+    
+    if(checkSuicide(color, x, y))
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+var checkSuicide = function(color, x, y)
+{
+    board[x][y] = color;
+    
+    var checkBoard = getCheckBoard();
+    
+    if(doCheckRemoveStone(color, x, y, checkBoard))
+    {
+        var opponet = (color == 1 ? 2 : 1);
+        
+        if(x > 0)
+        {
+            if(board[x - 1][y] == opponet)
+            {
+                checkBoard = getCheckBoard();
+                if(doCheckRemoveStone(opponet, x - 1, y, checkBoard))
+                {
+                    board[x][y] = 0;
+                    return false;
+                }
+            }
+        }
+        if(y > 0)
+        {
+            if(board[x][y - 1] == opponet)
+            {
+                checkBoard = getCheckBoard();
+                if(doCheckRemoveStone(opponet, x, y - 1, checkBoard))
+                {
+                    board[x][y] = 0;
+                    return false;
+                }
+            }
+        }
+        if(x < board.length - 1)
+        {
+            if(board[x + 1][y] == opponet)
+            {
+                checkBoard = getCheckBoard();
+                if(doCheckRemoveStone(opponet, x + 1, y, checkBoard))
+                {
+                    board[x][y] = 0;
+                    return false;
+                }
+            }
+        }
+        if(y < board.length - 1)
+        {
+            if(board[x][y + 1] == opponet)
+            {
+                checkBoard = getCheckBoard();
+                if(doCheckRemoveStone(opponet, x, y + 1, checkBoard))
+                {
+                    board[x][y] = 0;
+                    return false;
+                }
+            }
+        }
+        board[x][y] = 0;
+        return true;
+    }
+    else
+    {
+        board[x][y] = 0;
+        return false;
+    }
+}
+
+var setStone = function(color, x, y)
+{
+    setBoard(color, x, y);
+    
+    var isKou = true;
+    if(x > 0)
+    {
+        if(board[x - 1, y] == color) isKou = false;
+    }
+    if(y > 0)
+    {
+        if(board[x, y - 1] == color) isKou = false;
+    }
+    if(x < board.length - 1)
+    {
+        if(board[x + 1, y] == color) isKou = false;
+    }
+    if(y < board[x].length - 1)
+    {
+        if(board[x, y + 1] == color) isKou = false;
+    }
+    
+    var prisonerL = 0;
+    var prisonerR = 0;
+    var prisonerU = 0;
+    var prisonerD = 0;
+    if(x > 0)
+    {
+        prisonerL = removeStone(color, x - 1, y);
+    }
+    if(y > 0)
+    {
+        prisonerU = removeStone(color, x, y - 1);
+    }
+    if(x < board.length - 1)
+    {
+        prisonerR = removeStone(color, x + 1, y);
+    }
+    if(y < board[x].length - 1)
+    {
+        prisonerD = removeStone(color, x, y + 1);
+    }
+    
+    var prisonerAll = prisonerL + prisonerR + prisonerU + prisonerD;
+    if(isKou && prisonerAll == 1)
+    {
+        kou.turn = getTurn();
+        if(prisonerL == 1)
+        {
+            kou.x = x - 1;
+            kou.y = y;
+        }
+        else if(prisonerR == 1)
+        {
+            kou.x = x + 1;
+            kou.y = y;
+        }
+        else if(prisonerU == 1)
+        {
+            kou.x = x;
+            kou.y = y - 1;
+        }
+        else if(prisonerD == 1)
+        {
+            kou.x = x;
+            kou.y = y + 1;
+        }
+    }
+    
+    if(prisonerAll > 0)
+    {
+        if(color = 1)
+        {
+            prisoner.black += prisonerAll;
+        }
+        else
+        {
+            prisoner.white += prisonerAll;
+        }
+    }
+}
+
+var setBoard = function(color, x, y)
+{
+    board[x][y] = color;
+    objStone[x][y].setFrame(color);
+}
+
+var removeStone = function(color, x, y)
+{
+    if(board[x][y]  ==  0 || board[x][y] == color)
+    {
+        return 0;
+    }
+    
+    var checkBoard = getCheckBoard();
+    
+    if(doCheckRemoveStone(board[x][y], x, y, checkBoard))
+    {
+        return doRemoveStone(board[x][y], x, y, 0);
+    }
+    return 0;
+}
+
+var getCheckBoard = function()
+{
+    var checkBoard = [];
+    for(var x = 0; x < board.length; x++)
+    {
+        checkBoard[x] = [];
+        for(var y = 0; y < board[x].length; y++)
+        {
+            checkBoard[x][y] = false;
+        }
+    }
+    return checkBoard;
+}
+
+var doCheckRemoveStone = function(color, x, y, checkBoard)
+{
+    if(checkBoard[x][y])
+    {
+        return true;
+    }
+    
+    checkBoard[x][y] = true;
+    
+    if(board[x][y] == 0)
+    {
+        return false;
+    }
+    
+    if(board[x][y] == color)
+    {
+        if(x > 0)
+        {
+            if(!doCheckRemoveStone(color, x - 1, y, checkBoard))
+            {
+                return false;
+            }
+        }
+        if(y > 0)
+        {
+            if(!doCheckRemoveStone(color, x, y - 1, checkBoard))
+            {
+                return false;
+            }
+        }
+        if(x < board.length - 1)
+        {
+            if(!doCheckRemoveStone(color, x + 1, y, checkBoard))
+            {
+                return false;
+            }
+        }
+        if(y < board[x].length - 1)
+        {
+            if(!doCheckRemoveStone(color, x, y + 1, checkBoard))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+var doRemoveStone = function(color, x, y, prisonerCnt)
+{
+    if(board[x][y] == color)
+    {
+        prisonerCnt++;
+        setBoard(0, x, y);
+        
+        if(x > 0)
+        {
+            prisonerCnt = doRemoveStone(color, x - 1, y, prisonerCnt);
+        }
+        if(y > 0)
+        {
+            prisonerCnt = doRemoveStone(color, x, y - 1, prisonerCnt);
+        }
+        if(x < board.length - 1)
+        {
+            prisonerCnt = doRemoveStone(color, x + 1, y, prisonerCnt);
+        }
+        if(y < board[x].length - 1)
+        {
+            prisonerCnt = doRemoveStone(color, x, y + 1, prisonerCnt);
+        }
+    }
+    return prisonerCnt;
 }
 
 /************************************************************
